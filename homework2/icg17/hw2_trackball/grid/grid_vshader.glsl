043153c45a442fab0683301341e2948d6613b513
Xhulio Hasani <XhulioH@gmail.com>
2017-03-16 03:06:21 +0100

    the horrible story of a colorful wave

--- homework2/icg17/hw2_trackball/grid/grid_vshader.glsl
+++ homework2/icg17/hw2_trackball/grid/grid_vshader.glsl
@@ -1,5 +1,5 @@
 #version 330
-
+//#include <GLFW/glfw3.h>
 in vec2 position;
 
 out vec2 uv;
@@ -8,13 +8,15 @@ uniform mat4 MVP;
 uniform float time;
 
 void main() {
+float speed = 300;
+//float time =  glfwGetTime();
     uv = (position + vec2(1.0, 1.0)) * 0.5;
 
     // convert the 2D position into 3D positions that all lay in a horizontal
     // plane.
     // TODO 6: animate the height of the grid points as a sine function of the
     // 'time' and the position ('uv') within the grid.
-    float height = 0.0;
+    float height = sin(radians(uv[0]/10)*time*speed)*sin(radians(uv[1]/10)*time*speed)/5;
     vec3 pos_3d = vec3(position.x, height, -position.y);
 
     gl_Position = MVP * vec4(pos_3d, 1.0);
