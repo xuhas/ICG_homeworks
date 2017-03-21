4eb5721edb596282dd64e0a1ae4dc52041fe7e29
marouanef <marouane.fahli@epfl.ch>
2017-03-21 20:51:55 +0100

    Add solution to the 2nd part of the toon shader.

--- homework3/icg17/hw3_shading/mesh/toon_fshader.glsl
+++ homework3/icg17/hw3_shading/mesh/toon_fshader.glsl
@@ -1,22 +1,16 @@
 #version 330
 
+in vec3 normal_mv;
+in vec3 light_dir;
+in vec3 view_dir;
+
 uniform vec3 La, Ld, Ls;
 uniform vec3 ka, kd, ks;
 uniform float alpha;
 uniform mat4 projection;
-uniform mat4 model;
-uniform mat4 view;
-uniform vec3 light_pos;
-
-in vec3 light_dir;
-in vec3 view_dir;
-in vec3 normal_mv;
-
+uniform sampler1D tex1D;
 
 out vec3 color;
-
-
-uniform sampler1D tex1D;
 
 void main() {
     color = vec3(0.0,0.0,0.0);
@@ -32,10 +26,11 @@ void main() {
     vec3 n = normalize(normal_mv);
     vec3 l = normalize(light_dir);
     float lambert = dot(n,l);
-    if(lambert > 0.0) {
-        color += Ld*kd*lambert;
-        vec3 v = normalize(view_dir);
-        vec3 r = reflect(-l,n);
-        color += Ls*ks*pow(max(dot(r,v), 0.0), alpha);
-    }
+    vec3 color_nl = texture(tex1D, max(dot(n,l), 0.0f)).rgb;
+    color += Ld*kd*color_nl;
+    vec3 v = normalize(view_dir);
+    vec3 r = reflect(-l,n);
+    float pow = pow(max(dot(r,v), 0.0f), alpha);
+    vec3 color_rv = texture(tex1D, pow).rgb;
+    color += Ls*ks*color_rv;
 }
