6f61dd8494cbc623c2f23a01b5918d5f053abf25
Xhulio Hasani <XhulioH@gmail.com>
2017-03-22 21:48:45 +0100

    Some modifs in toon, better but not 100%

--- homework3/icg17/hw3_shading/mesh/toon_fshader.glsl
+++ homework3/icg17/hw3_shading/mesh/toon_fshader.glsl
@@ -13,7 +13,7 @@ uniform sampler1D tex1D;
 out vec3 color;
 
 void main() {
-   // color = vec3(0.0,0.0,0.0);
+   color = vec3(0.0,0.0,0.0);
 
     ///>>>>>>>>>> TODO >>>>>>>>>>>
     /// TODO 2.2: Toon shading.
@@ -22,15 +22,32 @@ void main() {
     /// 3) compute specular term using the texture sampler tex.
     ///<<<<<<<<<< TODO <<<<<<<<<<<
 
-    color += La*ka;
+//    color += La*ka;
+//    vec3 n = normalize(normal_mv);
+//    vec3 l = normalize(light_dir);
+//    float lambert = dot(n,l);
+//    vec3 color_nl = texture(tex1D, max(dot(n,l), 0.0f)).rgb;
+//    color += Ld*kd*color_nl;
+//    vec3 v = normalize(view_dir);
+//    vec3 r = reflect(-l,n);
+
+
+//    color += Ls*ks*color_rv*100;
+
     vec3 n = normalize(normal_mv);
     vec3 l = normalize(light_dir);
     float lambert = dot(n,l);
-    vec3 color_nl = texture(tex1D, max(dot(n,l), 0.0f)).rgb;
-    color += Ld*kd*color_nl;
-    vec3 v = normalize(view_dir);
-    vec3 r = reflect(-l,n);
-    float pow = pow(max(dot(r,v), 0.0f), alpha);
-    vec3 color_rv = texture(tex1D, pow).rgb;
-    color += Ls*ks*color_rv*100;
+    vec3 color_nl = texture(tex1D, max(lambert, 0.0f)).rgb;
+    color += La*ka;
+    if(lambert > 0.0) {
+        color += Ld*kd*color_nl;
+
+        vec3 v = normalize(view_dir);
+        vec3 r = reflect(-l,n);
+        float pow = pow(max(dot(r,v), 0.0f), alpha);
+        vec3 color_rv = texture(tex1D, pow).rgb;
+        //color += Ls*ks*color_rv;
+        color +=Ls*ks*pow(max(dot(r,v), 0.0), alpha);
+        color +=color_rv*color_nl;
+    }
 }
