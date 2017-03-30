87baf16d65981b5cb74469cbee9e812bbc46af74
marouanef <marouane.fahli@epfl.ch>
2017-03-30 05:34:58 +0200

    add quad fshader

--- homework4/icg17/hw4p3_motionblur/quad/quad_fshader.glsl
+++ homework4/icg17/hw4p3_motionblur/quad/quad_fshader.glsl
@@ -14,5 +14,11 @@ void main() {
     // TODO: compute the screen space motion vector (in pixels!)
     // HINT: use straightforward finite differences and assume unit time-step
     // HINT: how do you compute pixels positions given homogeneous coordinate? (x,y,z,w)
-    motionVector = vec2(0);
+    vec4 current_xyzw = currentPosition.xyzw;
+    vec3 current_cube = vec3(current_xyzw.x / current_xyzw.w, current_xyzw.y / current_xyzw.w, current_xyzw.z / current_xyzw.w).xyx;
+    vec2 current_screen = vec2(1024 * ((current_cube.x + 1) / 2), 768 * ((current_cube.y + 1) / 2)).xy;
+    vec4 previous_xyzw = previousPosition.xyzw;
+    vec3 previous_cube = vec3(previous_xyzw.x / previous_xyzw.w, previous_xyzw.y / previous_xyzw.w, previous_xyzw.z / previous_xyzw.w).xyx;
+    vec2 previous_screen = vec2(1024 * ((previous_cube.x + 1) / 2), 768 * ((previous_cube.y + 1) / 2)).xy;
+    motionVector = current_screen - previous_screen;
 }
